9ffb09fb944f1c55ac19575aa7689c20c3eeafe7
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2020-05-31 17:45:09 -0500

    Initial xcode project creation

---
+++ youtube-ios-mv/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-ios-mv
+//
+//  Created by Luis Rivera Alvarado on 31/05/20.
+//  Copyright © 2020 Luis Rivera Alvarado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
